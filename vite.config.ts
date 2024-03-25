1d1892a5f142f11a38a82299045e7d89f6789147
Mikael Björke <contact@mbjorke.com>
2024-03-25 19:30:13 +0200

    Auto import pascal case Quasar components

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
-      sassVariables: 'src/styles/quasar-variables.sass'
+      sassVariables: 'src/styles/quasar-variables.sass',
+      autoImportComponentCase: 'pascal'
     })
   ],
   resolve: {
