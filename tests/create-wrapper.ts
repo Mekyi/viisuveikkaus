776de209fda0c0af630998d8db01f6142d19871b
Mikael Björke <contact@mbjorke.com>
2024-04-03 01:59:20 +0300

    Setup i18n for unit test custom wrapper

--- tests/create-wrapper.ts
+++ tests/create-wrapper.ts
@@ -2,11 +2,24 @@ import { mount } from '@vue/test-utils'
 import router from '../src/router'
 
 import type { Component } from 'vue'
+import { createI18n } from 'vue-i18n'
+import en from '../src/i18n/en.json'
+import fi from '../src/i18n/fi.json'
+type MessageSchema = typeof en
 
 const createWrapper = (component: Component, options = {}) => {
+  const i18n = createI18n<[MessageSchema], 'en' | 'fi'>({
+    locale: 'fi',
+    fallbackLocale: 'en',
+    messages: {
+      en: en,
+      fi: fi
+    }
+  })
+
   return mount(component, {
     global: {
-      plugins: [router]
+      plugins: [router, i18n]
     },
     mocks: {
       $t: (msg: String) => msg
