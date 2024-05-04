9a5017d068132a4b20a5d6ebc6efdb3ff8b2752c
Mikael Björke <contact@mbjorke.com>
2024-05-04 18:55:15 +0300

    Preview for countries

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from '@/layouts/MainLayout.vue'
 import RatingView from '@/views/RatingView.vue'
 import PredictionView from '@/views/PredictionView.vue'
+import CountriesView from '@/views/CountriesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +12,8 @@ const router = createRouter({
       component: MainLayout,
       children: [
         { path: 'rating', component: RatingView, alias: '/' },
-        { path: 'prediction', component: PredictionView }
+        { path: 'prediction', component: PredictionView },
+        { path: 'countries', component: CountriesView }
       ]
     }
   ]
---
+++ src/views/CountriesView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import countriesData from '@/data/countries.json'
+import CountryFlag from 'vue-country-flag-next'
+
+// const countries: Country[] = countriesData as Country[]
+</script>
+<template>
+  <div
+    v-for="country in countriesData"
+    :key="country.code"
+  >
+    <QCard class="q-my-md">
+      <QCardSection
+        class="text-white"
+        style="background: linear-gradient(to right, #3f1c71, #9c27b0)"
+      >
+        <div class="text-h5 vertical-middle">
+          <CountryFlag
+            :country="country.code"
+            size="normal"
+          />
+          <span class="q-ml-md">{{ country.name.toUpperCase() }}</span>
+        </div>
+      </QCardSection>
+    </QCard>
+  </div>
+</template>
