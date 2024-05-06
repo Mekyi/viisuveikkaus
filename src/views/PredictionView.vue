3cdac1c459f00a51c4848fd3ed3aee0c6fa1e953
Mikael Björke <contact@mbjorke.com>
2024-05-06 11:41:28 +0300

    Install Vue draggable

--- src/views/PredictionView.vue
+++ src/views/PredictionView.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+import ContestantCard from '@/components/ContestantCard.vue'
+import { useContestsStore } from '@/stores/contests'
+import { type Contestant } from '@/ts/types/contestant'
+import { onMounted, ref } from 'vue'
+import draggable from 'vuedraggable'
+
+const dragList = ref<Contestant[] | undefined>()
+const contestsStore = useContestsStore()
+const drag = ref(false)
+
+onMounted(() => {
+  dragList.value = contestsStore.availableShows.find(
+    (show) => show.showType === contestsStore.selectedShow
+  )?.contestants
+})
+</script>
+
 <template>
-  <h1>Prediction</h1>
+  <draggable
+    :list="dragList"
+    item-key="country"
+    @start="drag = true"
+    @end="drag = false"
+  >
+    <template #item="{ element }">
+      <ContestantCard :contestant="element" />
+    </template>
+  </draggable>
 </template>
