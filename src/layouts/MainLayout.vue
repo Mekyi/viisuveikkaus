3f6657b4cd867d2821c5499c28c3b7ed845f9f46
Mikael Björke <contact@mbjorke.com>
2024-05-07 19:24:44 +0300

    Navigation styles and UX

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,37 @@
 <script setup lang="ts">
 import { useContestsStore } from '@/stores/contests'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import logoUrl from '@/assets/logo.svg'
+import { useI18n } from 'vue-i18n'
+import type ShowFormat from '@/ts/enums/showFormat'
 
 const toggleSideMenu = ref(false)
 const contestsStore = useContestsStore()
+
+const { locale } = useI18n({ useScope: 'global' })
+const lang = ref(locale.value)
+const localeOptions = [
+  { value: 'en', label: 'English' },
+  { value: 'fi', label: 'suomi' }
+]
+
+watch(lang, async (newLang: any) => {
+  locale.value = newLang.value
+})
 
 function clearContestStorage() {
   contestsStore.contests = null
   location.reload()
 }
+
+function selectShow(showFormat: ShowFormat) {
+  contestsStore.selectedShow = showFormat
+  toggleSideMenu.value = false
+}
 </script>
 
 <template>
-  <QLayout view="hHh Lpr fFf">
+  <QLayout view="hHh Lpr lFf">
     <QHeader elevated>
       <QToolbar>
         <QBtn
@@ -20,9 +39,11 @@ function clearContestStorage() {
           round
           dense
           icon="menu"
+          class="q-mr-sm"
           @click="toggleSideMenu = !toggleSideMenu"
         />
-        <QToolbarTitle>
+        <QToolbarTitle style="font-size: 18px">
+          {{ $t('title') }}
           {{ contestsStore.selectedYear }} -
           {{ $t(`showType[${contestsStore.selectedShow}]`) }}
         </QToolbarTitle>
@@ -35,8 +56,9 @@ function clearContestStorage() {
           <QMenu>
             <QItem>
               <QSelect
-                v-model="$i18n.locale"
-                :options="$i18n.availableLocales"
+                v-model="lang"
+                :options="localeOptions"
+                map-options
                 style="min-width: 150px"
                 borderless
               >
@@ -51,6 +73,13 @@ function clearContestStorage() {
               clickable
               @click="clearContestStorage"
             >
+              <QItemSection
+                avatar
+                dense
+                round
+              >
+                <QIcon name="delete" />
+              </QItemSection>
               <QItemSection>{{ $t('common.clear') }}</QItemSection>
             </QItem>
           </QMenu>
@@ -60,22 +89,42 @@ function clearContestStorage() {
 
     <QDrawer
       v-model="toggleSideMenu"
-      show-if-above
-      :breakpoint="500"
+      :breakpoint="800"
       bordered
       overlay
     >
       <QScrollArea class="fit">
-        <QList bordered>
+        <QToolbar class="bg-primary">
+          <QAvatar square>
+            <img :src="logoUrl" />
+          </QAvatar>
+          <QToolbarTitle class="text-h5">
+            {{ $t('title') }}
+          </QToolbarTitle>
+          <QBtn
+            dense
+            flat
+            unelevated
+            icon="chevron_left"
+            @click="toggleSideMenu = false"
+          />
+        </QToolbar>
+        <QSeparator />
+        <QList>
           <QItem
             v-for="show in contestsStore.availableShows"
             :key="show.showType"
             v-ripple
             clickable
-            @click="contestsStore.selectedShow = show.showType"
+            :active="show.showType == contestsStore.selectedShow"
+            @click="selectShow(show.showType)"
           >
+            <QItemSection avatar>
+              <QIcon name="navigate_next" />
+            </QItemSection>
             <QItemSection>
-              {{ $t(`showType[${contestsStore.selectedShow}]`) }} - {{ show.date }}
+              <QItemLabel class="text-6">{{ $t(`showType[${show.showType}]`) }}</QItemLabel>
+              <QItemLabel caption>{{ show.date }} </QItemLabel>
             </QItemSection>
           </QItem>
         </QList>
