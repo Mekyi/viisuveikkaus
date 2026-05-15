db567be90621f12821856979cc1a58d433ea758e
Mikael Björke <contact@mbjorke.com>
2026-05-15 18:26:51 +0300

    Default year to 2026

--- src/stores/contests.ts
+++ src/stores/contests.ts
@@ -7,8 +7,8 @@ import { useStorage } from '@vueuse/core'
 
 export const useContestsStore = defineStore('contests', () => {
   const contests = useStorage<Contest[] | null>('contestsLocal', contestsData, localStorage)
-  const selectedYear = ref<number>(2025)
-  const selectedShow = ref<ShowFormat | null>(ShowFormat.FirstSemiFinal)
+  const selectedYear = ref<number>(2026)
+  const selectedShow = ref<ShowFormat | null>(ShowFormat.Final)
 
   const contestants = computed(() => {
     const foundContest = contests.value?.find((contest) => contest.year === selectedYear.value)
